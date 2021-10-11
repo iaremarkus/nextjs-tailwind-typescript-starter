d2fc49d4849d4a6b9fdc459f531c56d92e3193de
Mark Bloomfield <mark@yellow-llama.com>
2021-10-11 20:42:06 +0200

    nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
+import NProgress from "nprogress";
+import Router from "next/router";
 import "../src/styles/global.css";
+import "nprogress/nprogress.css";
+
 
 import type { AppProps } from "next/app";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
